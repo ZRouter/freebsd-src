353b6c1a9acf9f1e02949904cebc36a6a58912f0
Hiroki Mori <yamori813@yahoo.co.jp>
2024-06-12 10:11:59 +0900

    comment out debug code

--- sys/mips/broadcom/bcm338x/obio.c
+++ sys/mips/broadcom/bcm338x/obio.c
@@ -212,7 +212,6 @@ obio_attach(device_t dev)
 	device_printf(dev, "Broadcom CHIP ID: %x\n",
 	    BCM_READ_REG(BCM3383_INTC_BASE));
 
-#endif
 	// Timer intr test
 //	int reg = BCM_READ_REG(BCM3383_INTC_BASE + 4 * (12 + 2 * 3));
 //	BCM_WRITE_REG(BCM3383_INTC_BASE + 4 * (12 + 2 * 3), reg | 1);
@@ -234,6 +233,7 @@ obio_attach(device_t dev)
 		}
 		printf("\n");
 	}
+#endif
 
 	bus_generic_probe(dev);
 	bus_enumerate_hinted_children(dev);
