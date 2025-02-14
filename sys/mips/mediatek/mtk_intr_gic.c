9d76de26963413a1a8c044c9b1f006896ccb30d8
Hiroki Mori <yamori813@yahoo.co.jp>
2025-02-14 16:53:16 +0900

    add comment

--- sys/mips/mediatek/mtk_intr_gic.c
+++ sys/mips/mediatek/mtk_intr_gic.c
@@ -209,7 +209,8 @@ mtk_gic_attach(device_t dev)
 #define VPE_OTHER_SECTION	0xc000
 #define GIC_VPE_SMASK		0x0010
 
-	/* Enable each VPE's CP0 Count/Compare Timer interrupt */
+	/* Enable each VPE's CP0 Count/Compare Timer interrupt
+	   This is default. No impuct. */
 	for (i = 0;i < 4; ++i) {
 		WRITE4(sc, VPE_LOCAL_SECTION + GIC_VPE_OTHER_ADDR, i);
 		WRITE4(sc, VPE_OTHER_SECTION + GIC_VPE_SMASK, 1 << 2);
